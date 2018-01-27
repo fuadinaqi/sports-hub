7336e0df20d75db89402b866765e9e66ed572a7c
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-01-27 23:58:53 +0700

    agenda findAll

--- routes/agenda.js
+++ routes/agenda.js
@@ -1,8 +1,15 @@
 const express     = require('express')
 const router      = express.Router()
+const Model       = require('../models')
 
 router.get('/', (req, res) => {
-  res.send('hola')
+  Model.Agenda.findAll()
+  .then(rowAgendas => {
+    res.send(rowAgendas)
+  })
+  .catch(err => {
+    res.send(err)
+  })
 })
 
 module.exports = router;
