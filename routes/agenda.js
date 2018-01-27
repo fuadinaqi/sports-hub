9aaf5e174f5dbaa8b32c751c6e4e0927a6f01226
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-01-28 00:57:09 +0700

    hard crud agenda done

--- routes/agenda.js
+++ routes/agenda.js
@@ -3,9 +3,79 @@ const router      = express.Router()
 const Model       = require('../models')
 
 router.get('/', (req, res) => {
-  Model.Agenda.findAll()
+  Model.Agenda.findAll() // select all data agendas
   .then(rowAgendas => {
-    res.send(rowAgendas)
+    res.render('agenda', {
+      rowAgendas : rowAgendas
+    })
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
+router.get('/add', (req, res) => {
+  res.render('agenda_add')
+  // sport id (list) nya belom
+})
+
+router.post('/add', (req, res) => {
+  let objCreate = {
+    name        : req.body.name,
+    place       : req.body.place,
+    date        : req.body.date,
+    time        : req.body.time,
+    max_player  : req.body.max_player
+  }
+  Model.Agenda.create(objCreate) //insert data agenda
+  .then(() => {
+    res.redirect('/agendas')
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
+router.get('/edit/:id', (req, res) => {
+  let id = req.params.id
+  Model.Agenda.findById(id)
+  .then(dataAgenda => {
+    res.render('agenda_edit', {
+      dataAgenda : dataAgenda
+    })
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
+router.post('/edit/:id', (req, res) => {
+  let id = req.params.id
+  let objEdit = {
+    name        : req.body.name,
+    place       : req.body.place,
+    date        : req.body.date,
+    time        : req.body.time,
+    max_player  : req.body.max_player
+  }
+  Model.Agenda.update(objEdit, { //update data agenda
+    where: { id }
+  })
+  .then(() => {
+    res.redirect('/agendas')
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
+router.get('/delete/:id', (req, res) => {
+  let id = req.params.id
+  Model.Agenda.destroy({ //delete data agenda
+    where : { id }
+  })
+  .then(() => {
+    res.redirect('/agendas')
   })
   .catch(err => {
     res.send(err)
