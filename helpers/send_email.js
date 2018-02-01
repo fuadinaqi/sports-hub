7074eed4fd9d8a9dadd9d27f25af975be441082b
Hacktiv8 <hacktiv8@H8JKT001.local>
2018-02-01 22:34:40 +0700

    email costumized

--- helpers/send_email.js
+++ helpers/send_email.js
@@ -2,13 +2,15 @@
 const api_key     = 'key-25fc0a73025b3329627740cc582f9021';
 const domain      = 'sandboxb33efd7116d9434ab04cbd7bc49c1833.mailgun.org';
 const mailgun     = require('mailgun-js')({apiKey: api_key, domain: domain});
+const emailJoin       = require('./email-join');
+const emailFull       = require('./email-full-booked');
 
 function sendEmailToUser(person,agenda) {
   var data = {
     from: 'Sports Hub Fuadhi <postmaster@sandboxb33efd7116d9434ab04cbd7bc49c1833.mailgun.org>',
     to: person.email,
     subject: 'Hello From Sports Hub!!',
-    text: `Congrats! Your Event ${agenda.name} is fully booked`
+    html: emailFull(person,agenda)
   };
   mailgun.messages().send(data, function (error, body) {
     console.log('body ..',body);
@@ -34,7 +36,7 @@ function sendEmailWhenJoin(person,agenda) {
     from: 'Sports Hub Fuadhi <postmaster@sandboxb33efd7116d9434ab04cbd7bc49c1833.mailgun.org>',
     to: person.email,
     subject: 'Hello From Sports Hub!!',
-    text: `Congrats! You have joined ${agenda.name}!! Please wait for further notification when the team is full`
+    html: emailJoin(person, agenda)
   };
   mailgun.messages().send(data, function (error, body) {
     console.log('body ..',body);
